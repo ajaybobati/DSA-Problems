57a132f6c9bffb1051d6ca197e291f80ddfc8bca
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-17 21:55:11 +0530

    added all

---
+++ Array/Maximum Sum Subarray Problem (Kadane’s Algorithm).java
@@ -0,0 +1,37 @@
+package Array;
+
+class Maxi
+{
+    
+    public static int kadane(int[] arr)
+    {
+        
+        int maxSoFar = 0;
+ 
+        
+        int maxEndingHere = 0;
+ 
+        // traverse the given array
+        for (int i: arr)
+        {
+            
+            maxEndingHere = maxEndingHere + i;
+ 
+            
+            maxEndingHere = Integer.max(maxEndingHere, 0);
+ 
+            maxSoFar = Integer.max(maxSoFar, maxEndingHere);
+        }
+ 
+        return maxSoFar;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+ 
+        System.out.println("The sum of contiguous subarray with the " +
+                            "largest sum is " + kadane(arr));
+    }
+}
+
