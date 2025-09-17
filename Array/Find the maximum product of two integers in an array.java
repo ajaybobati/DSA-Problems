6220216c2e8529fb1ea5b8bd8c1877d477fffe56
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-17 17:29:21 +0530

    added find

---
+++ Array/Find the maximum product of two integers in an array.java
@@ -0,0 +1,39 @@
+package Array;
+class findMaximum
+{
+    
+    public static void findMaximumProduct(int[] A)
+    {
+        
+        if (A.length < 2) {
+            return;
+        }
+ 
+        int max_product = Integer.MIN_VALUE;
+        int max_i = -1, max_j = -1;
+ 
+        
+        for (int i = 0; i < A.length - 1; i++)
+        {
+            for (int j = i + 1; j < A.length; j++)
+            {
+                
+                if (max_product < A[i] * A[j])
+                {
+                    max_product = A[i] * A[j];
+                    max_i = i;
+                    max_j = j;
+                }
+            }
+        }
+ 
+        System.out.print("Pair is (" + A[max_i] + ", " + A[max_j] + ")");
+    }
+ 
+    public static void main (String[] args)
+    {
+        int[] A = { -10, -3, 5, 6, -2 };
+ 
+        findMaximumProduct(A);
+    }
+}
