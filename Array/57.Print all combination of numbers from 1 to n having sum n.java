6638ff0d4b6a06274d7beb59f553e4a2229ab281
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-19 23:04:19 +0530

    added all

---
+++ Array/57.Print all combination of numbers from 1 to n having sum n.java
@@ -0,0 +1,29 @@
+package Array;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+ 
+class printCombin
+{
+    public static void printCombinations(int i, int n, int[] out, int index)
+    {
+        if (n == 0)
+        {
+            System.out.println(Arrays.stream(out).limit(index)
+                    .boxed().collect(Collectors.toList()));
+        }
+        for (int j = i; j <= n; j++)
+        {
+            out[index] = j;
+ 
+            printCombinations(j, n - j, out, index + 1);
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        int n = 5;
+        int[] out = new int[n];
+ 
+        printCombinations(1, n, out, 0);
+    }
+}
