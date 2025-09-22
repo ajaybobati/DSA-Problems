4abc41b3d4692ee99abedc474a39dd001f2c72fd
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-22 18:36:12 +0530

    added all

---
+++ Array/104.Bubble sort  Iterative & Recursive.java
@@ -0,0 +1,35 @@
+package Array;
+import java.util.Arrays;
+ 
+class Bubbleso
+{
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+ 
+    public static void bubbleSort(int[] arr, int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr, i, i + 1);
+            }
+        }
+ 
+        if (n - 1 > 1) {
+            bubbleSort(arr, n - 1);
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] arr = { 3, 5, 8, 4, 1, 9, -2 };
+ 
+        bubbleSort(arr, arr.length);
+ 
+        System.out.println(Arrays.toString(arr));
+    }
+}
