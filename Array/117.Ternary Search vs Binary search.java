77ed170fd25929fe5bcbe5e9584fa4ea2819e3d7
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-22 22:43:26 +0530

    added all

---
+++ Array/117.Ternary Search vs Binary search.java
@@ -0,0 +1,49 @@
+package Array;
+class ternaryS
+{
+
+    public static int ternarySearch(int[] A, int x)
+    {
+        int left = 0, right = A.length - 1;
+ 
+        while (left <= right)
+        {
+            int leftMid = left + (right - left) / 3;
+            int rightMid = right - (right - left) / 3;
+ 
+ 
+            if (A[leftMid] == x) {
+                return leftMid;
+            }
+            else if (A[rightMid] == x) {
+                return rightMid;
+            }
+            else if (A[leftMid] > x) {
+                right = leftMid - 1;
+            }
+            else if (A[rightMid] < x) {
+                left = rightMid + 1;
+            }
+            else {
+                left = leftMid + 1;
+                right = rightMid - 1;
+            }
+        }
+        return -1;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] A = { 2, 5, 6, 8, 9, 10 };
+        int key = 6;
+ 
+        int index = ternarySearch(A, key);
+ 
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        }
+        else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
