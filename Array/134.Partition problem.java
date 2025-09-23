c98167ed5d1a8a04f336859d9dc362aed6785eff
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-23 17:10:02 +0530

    addded all

---
+++ Array/134.Partition problem.java
@@ -0,0 +1,43 @@
+package Array;
+
+
+import java.util.Arrays;
+ 
+class subsetS
+{
+    public static boolean subsetSum(int[] nums, int n, int sum)
+    {
+        if (sum == 0) {
+            return true;
+        }
+ 
+        if (n < 0 || sum < 0) {
+            return false;
+        }
+        boolean include = subsetSum(nums, n - 1, sum - nums[n]);
+ 
+        if (include) {
+            return true;
+        }
+        boolean exclude = subsetSum(nums, n - 1, sum);
+ 
+        return exclude;
+    }
+    public static boolean partition(int[] nums)
+    {
+        int sum = Arrays.stream(nums).sum();
+        return (sum & 1) == 0 && subsetSum(nums, nums.length - 1, sum/2);
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] nums = { 7, 3, 1, 5, 4, 8 };
+ 
+        if (partition(nums)) {
+            System.out.println("Set can be partitioned");
+        }
+        else {
+            System.out.println("Set cannot be partitioned");
+        }
+    }
+}
---
+++ Array/135.Subset sum problem.java
@@ -0,0 +1,31 @@
+package Array;
+class Main
+{
+    public static boolean subsetSum(int[] A, int n, int k)
+    {
+        if (k == 0) {
+            return true;
+        }
+        if (n < 0 || k < 0) {
+            return false;
+        }
+        boolean include = subsetSum(A, n - 1, k - A[n]);
+        boolean exclude = subsetSum(A, n - 1, k);
+ 
+        return include || exclude;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] A = { 7, 3, 2, 5, 8 };
+        int k = 14;
+ 
+        if (subsetSum(A, A.length - 1, k)) {
+            System.out.print("Subsequence with the given sum exists");
+        }
+        else {
+            System.out.print("Subsequence with the given sum does not exist");
+        }
+    }
+}
+
