040f84d575b535980e5ef0c410a5d36997c97779
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-23 22:25:20 +0530

    added all

---
+++ Array/150.3-partition problem extended  Print all partitions.java
@@ -0,0 +1,72 @@
+package Array;
+
+import java.util.Arrays;
+ 
+class isSub
+{
+    public static boolean isSubsetExist(int[] S, int n, int a, int b, int c, int[] arr)
+    {
+        if (a == 0 && b == 0 && c == 0) {
+            return true;
+        }
+        if (n < 0) {
+            return false;
+        }
+        boolean A = false;
+        if (a - S[n] >= 0)
+        {
+            arr[n] = 1;       
+            A = isSubsetExist(S, n - 1, a - S[n], b, c, arr);
+        }
+ 
+        boolean B = false;
+        if (!A && (b - S[n] >= 0))
+        {
+            arr[n] = 2;        
+            B = isSubsetExist(S, n - 1, a, b - S[n], c, arr);
+        }
+ 
+        boolean C = false;
+        if ((!A && !B) && (c - S[n] >= 0))
+        {
+            arr[n] = 3;        
+            C = isSubsetExist(S, n - 1, a, b, c - S[n], arr);
+        }
+ 
+        return A || B || C;
+    }
+    public static void partition(int[] S)
+    {
+        int sum = Arrays.stream(S).sum();
+ 
+        int[] A = new int[S.length];
+ 
+        boolean result = (S.length >= 3) &&
+                    (sum % 3) == 0 &&
+                    isSubsetExist(S, S.length - 1, sum/3, sum/3, sum/3, A);
+ 
+        if (!result)
+        {
+            System.out.print("3-Partition of set is not possible");
+            return;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            System.out.print("Partition " + i + " is ");
+            for (int j = 0; j < S.length; j++)
+            {
+                if (A[j] == i + 1) {
+                    System.out.print(S[j] + " ");
+                }
+            }
+            System.out.print(System.lineSeparator());
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] S = { 7, 3, 2, 1, 5, 4, 8 };
+        partition(S);
+    }
+}
+
---
+++ Array/152.Find two duplicate elements in an limited range array (using XOR).java
@@ -0,0 +1,49 @@
+
+package Array;
+class finddup
+{
+    public static int log(int x, int base) {
+        return (int) (Math.log(x) / Math.log(base));
+    }
+ 
+    public static void findDuplicates(int[] arr, int n)
+    {
+        int result = arr[0] ^ arr[n + 1];
+        for (int i = 1; i <= n; i++) {
+            result = result ^ arr[i] ^ i;
+        }
+        int x = 0, y = 0;
+        int k = log(result & -result, 2);
+ 
+        for (int i = 0; i < n + 2; i++)
+        {
+            if ((arr[i] & (1 << k)) != 0) {
+                x = x ^ arr[i];
+            }
+            else {
+                y = y ^ arr[i];
+            }
+        }
+ 
+        for (int i = 1; i <= n; i++)
+        {
+            if ((i & (1 << k)) != 0) {
+                x = x ^ i;
+            }
+            else {
+                y = y ^ i;
+            }
+        }
+ 
+        System.out.println("The duplicate elements are " + x + " and " + y);
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] arr = { 4, 3, 6, 5, 2, 4, 1, 1 };
+        int n = 6;      
+ 
+        findDuplicates(arr, n);
+    }
+}
+
