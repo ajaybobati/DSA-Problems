2f22c998f4c560e77f1bbb68dadd76f09b94628b
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-24 18:16:48 +0530

    added all

---
+++ BinaryTree/1.Bit Hacks — Part 1 (Basic).java
@@ -0,0 +1,17 @@
+package BinaryTree;
+
+class Hacks1
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+ 
+        if ((n & 1) != 0) {
+            System.out.println(n + " is odd");
+        }
+        else {
+            System.out.println(n + " is even");
+        }
+    }
+}
+
---
+++ BinaryTree/2.Bit Hacks — Part 2 (Playing with k’th bit).java
@@ -0,0 +1,18 @@
+package BinaryTree;
+class hacks2
+{
+    public static int turnOffKthBit(int n, int k) {
+        return n & ~(1 << (k - 1));
+    }
+ 
+    public static void main(String[] args)
+    {
+        int n = 20;
+        int k = 3;
+ 
+        System.out.println(n + " in binary is " + Integer.toBinaryString(n));
+        System.out.println("Turning k'th bit off…");
+        n = turnOffKthBit(n, k);
+        System.out.println(n + " in binary is " + Integer.toBinaryString(n));
+    }
+}
