3aac293d8247dff5f38faf935587a31c951d84ac
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-24 20:16:22 +0530

    added all

---
+++ BinaryTree/12.Round up to the next highest power of 2.java
@@ -0,0 +1,22 @@
+package BinaryTree;
+
+
+class Roundup
+{
+    public static int findNextPowerOf2(int n)
+    {
+        n = n - 1;
+ 
+        while ((n & n - 1) != 0) {
+            n = n & n - 1;      
+        }
+        return n << 1;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int n = 127;
+ 
+        System.out.println("The next power of 2 is " + findNextPowerOf2(n));
+    }
+}
