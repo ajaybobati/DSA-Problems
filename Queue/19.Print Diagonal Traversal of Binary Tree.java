16b5cddb68d3e6ca5262dd2adc1302a9b0eef2d6
Ajaybobati <ajaylucky9009@gmail.com>
2025-09-27 21:28:19 +0530

    added all

---
+++ Queue/19.Print Diagonal Traversal of Binary Tree.java
@@ -0,0 +1,58 @@
+package Queue;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+class Node
+{
+    int data;
+    Node left = null, right = null;
+ 
+    Node(int data) {
+        this.data = data;
+    }
+}
+ 
+class Main
+{
+    public static void printDiagonal(Node node, int diagonal,
+                                    Map<Integer, List<Integer>> map)
+    {
+        if (node == null) {
+            return;
+        }
+        map.putIfAbsent(diagonal, new ArrayList<>());
+        map.get(diagonal).add(node.data);
+ 
+        printDiagonal(node.left, diagonal + 1, map);
+        printDiagonal(node.right, diagonal, map);
+    }
+    public static void printDiagonal(Node root)
+    {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+ 
+        printDiagonal(root, 0, map);
+ 
+        for (int i = 0; i < map.size(); i++) {
+            System.out.println(map.get(i));
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+ 
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.left = new Node(7);
+        root.right.left.right = new Node(8);
+ 
+        printDiagonal(root);
+    }
+}
+
---
+++ Queue/20.Print corner nodes of every level in binary tree.java
@@ -0,0 +1,66 @@
+package Queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+class Node
+{
+    int data;
+    Node left = null, right = null;
+ 
+    Node(int data) {
+        this.data = data;
+    }
+}
+ 
+class Main
+{
+    public static void print(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+
+
+        Queue<Node> q = new ArrayDeque<>();
+ 
+        q.add(root);
+        while (!q.isEmpty())
+        {
+            int size = q.size();
+            int n = size;
+            while (n-- > 0)
+            {
+                Node node = q.poll();
+                if (n == size - 1 || n == 0) {
+                    System.out.print(node.data + " ");
+                }
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+ 
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            System.out.println();
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.left.left.left = new Node(7);
+        root.right.left.left = new Node(8);
+        root.right.left.right = new Node(9);
+        root.right.right.right = new Node(10);
+ 
+        print(root);
+    }
+}
+
+
